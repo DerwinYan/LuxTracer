66aa4211284441b803ba4f414d7286ca1876044c
DerwinYan <derwinyan25@gmail.com>
2025-11-15 15:37:05 +0800

    raytrace sphere

--- StaticTracer/StaticTracer/src/Graphics/Renderer.cpp
+++ StaticTracer/StaticTracer/src/Graphics/Renderer.cpp
@@ -5,9 +5,6 @@
 #include <Math/vec3.h>
 #include <Graphics/Ray.h>
 
-pt::Renderer::Renderer(Window _window) 
-  : window{ _window }, framebuffer{ window.getWidth(), window.getHeight() }
-{}
 
 math::vec3 skyboxColor(pt::Ray const& ray)
 {
@@ -18,6 +15,45 @@ math::vec3 skyboxColor(pt::Ray const& ray)
   float t{ 0.5f * (direction.y + 1.0f) };
   return math::lerp(skyColor, horizonColor, t);
 }
+
+bool hitSphere(math::vec3 const& center, float radius, pt::Ray const& ray)
+{
+  // Point from vector: Q + t * vector
+  // Sphere formula: 
+  // [C-P]*[C-P} = r^2
+  // [C- (Q+tv) ]*[C- (Q+tv) ] = r^2
+  // {t^2 * v*v} + {2tv(C-Q)} + {(C-Q)^2 - r^2} = 0
+  //     a            b               c
+
+  math::vec3 CQvec{ center - ray.getOrigin() };
+
+  // Quadratic equation formula
+  float a{ math::dot(ray.getDirection(), ray.getDirection()) };
+  float b{ 2.0f * math::dot(ray.getDirection(), CQvec) };
+  float c{ math::dot(CQvec, CQvec) - radius * radius };
+
+  float discriminant{ b*b - 4*a*c };
+  return discriminant >= 0.0f;
+}
+
+math::vec3 traceRay(pt::Ray const& ray)
+{
+  static const math::vec3 sphereCenter{ 0,0,-1 };
+  static const float radius{ 0.5 };
+
+  if (hitSphere(sphereCenter, radius, ray))
+  {
+    return { 1,0,0 };
+  }
+  else
+  {
+    return skyboxColor(ray);
+  }
+}
+
+pt::Renderer::Renderer(Window _window) 
+  : window{ _window }, framebuffer{ window.getWidth(), window.getHeight() }
+{}
 
 void pt::Renderer::render()
 {
@@ -44,7 +80,7 @@ void pt::Renderer::render()
       math::vec3 rayDirection{ pixelCenter - cameraPosition };
       pt::Ray ray{ cameraPosition, rayDirection };
 
-      Pixel pixel{ skyboxColor(ray) };
+      Pixel pixel{ traceRay(ray) };
 
       framebuffer.setPixel(x, y, pixel);
     }
